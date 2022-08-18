5afac56573a968dddfae495f35c08b129962b2a1
Nurafni Kurniawati <nrafnikr900@gmail.com>
2022-08-18 20:37:46 +0700

    Task Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,11 @@
-import './App.css';
+// import './App.css';
+import Redux from './Task/Redux';
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import Navbar from './Task/Styling/Navbar';
-import Masthead from './Task/Styling/Masthead';
-import Skill from './Task/Styling/Skill';
-import Organization from './Task/Styling/Organization';
-import Contact from './Task/Styling/Contact';
-import Footer from './Task/Styling/Footer';
-import About from './Task/Styling/About';
 
 const App = () => {
   return (
     <>
-      <Navbar />
-      <Masthead />
-      <Skill />
-      <Organization />
-      <About />
-      <Contact />
-      <Footer /> 
+      <Redux /> 
     </>
   );
 }
